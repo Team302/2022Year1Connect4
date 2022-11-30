4aa8f5b1479e855888d734a43e6dec9d1248c99c
Charlie Cairo <locairoc40@stu.lok12.org>
2022-11-29 20:29:33 -0500

    flag states in-progress

---
+++ src/main/cpp/mechanisms/flag/Flagstatemanager.cpp
@@ -0,0 +1,102 @@
+//====================================================================================================================================================
+/// Copyright 2022 Lake Orion Robotics FIRST Team 302 
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+/// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+/// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
+#include <map>
+#include <memory>
+#include <vector>
+
+// FRC includes
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableEntry.h>
+
+// Team 302 includes
+#include <mechanisms/controllers/MechanismTargetData.h>
+#include <TeleopControl.h>
+#include <mechanisms/MechanismFactory.h>
+#include <mechanisms/MechanismTypes.h>
+#include <mechanisms/base/StateMgr.h>
+#include <mechanisms/StateStruc.h>
+#include <mechanisms/base/IState.h>
+#include <utils/Logger.h>
+#include <mechanisms/controllers/StateDataXmlParser.h>
+#include <mechanisms/flag/Flagstatemanager.h>
+
+// Third Party Includes
+
+using namespace std;
+
+
+Flagstatemanager* Flagstatemanager::m_instance = nullptr;
+Flagstatemanager* Flagstatemanager::GetInstance()
+{
+	if ( FlagStatemanager::m_instance == nullptr )
+	{
+	    auto mechFactory = MechanismFactory::GetMechanismFactory();
+	    auto flagarm = mechFactory->GetFlag();
+	    if (flagarm != nullptr)
+        {
+		    FlagStatemanager::m_instance = new FlagStatemanager();
+        }
+	}
+	return FlagStatemanager::m_instance;
+    
+}
+
+
+/// @brief    initialize the state manager, parse the configuration file and create the states.
+Flagstatemanager::Flagstatemanager() : StateMgr(),
+                                     m_Flag(MechanismFactory::GetMechanismFactory()->GetFlag()),
+                                     m_nt()
+{
+    map<string, StateStruc> stateMap;
+    stateMap[Flagstatemanager::Open] = m_ClosedState;
+    stateMap[Flagstatemanager::Open] = m_OpenState; 
+
+    Init(m_Flagarm, stateMap);
+    if (m_Flagarm != nullptr)
+    {
+        auto m_nt = m_Flagarm->GetNetworkTableName();
+    }
+}   
+
+/// @brief Check if driver inputs or sensors trigger a state transition
+void ExampleStateMgr::CheckForStateTransition()
+{
+
+    if ( m_Flag != nullptr )
+    {    
+        auto currentState = static_cast<FLAG_STATE>(GetCurrentState());
+        auto targetState = currentState;
+
+        auto controller = TeleopControl::GetInstance();
+        auto isForwardSelected   = controller != nullptr ? controller->IsButtonPressed(TeleopControl::FLAG_STATE::Open) : true;
+        auto isReverseSelected   = controller != nullptr ? controller->IsButtonPressed(TeleopControl::FLAG_STATE::Closed) : false;
+
+        if (isForwardSelected)
+        {
+            targetState = FLAG_ARM::Open;
+        }
+        else
+        {
+            targetState = FLAG_ARM::Closed;
+        }
+
+        if (targetState != currentState)
+        {
+            SetCurrentState(targetState, true);
+        }
+        
+    }
+}
--- src/main/cpp/mechanisms/flag/Flagstatemanager.h
+++ src/main/cpp/mechanisms/flag/Flagstatemanager.h
@@ -1,12 +1,73 @@
-class flag : public flag
+//====================================================================================================================================================
+/// Copyright 2022 Lake Orion Robotics FIRST Team 302 
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+/// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+/// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+/// OR OTHER DEALINGS IN THE SOFTWARE.
+//====================================================================================================================================================
 
+#pragma once
+
+// C++ Includes
+#include <string>
+
+// FRC includes
+#include <networktables/NetworkTable.h>
+#include <frc/Timer.h>
+
+// Team 302 includes
+#include <mechanisms/base/StateMgr.h>
+#include <mechanisms/example/Example.h>
+#include <mechanisms/StateStruc.h>
+
+
+
+
+// Third Party Includes
+class Flagarm;
+
+class Flagstatemanager : public StateMgr
+{
     public:
+        /// @enum the various states of the Intake
+        enum FLAG_ARM
+        {
+            Open,
+            Closed
+            
+        };
+        const std::string m_OpenXmlString = "Open";
+        const std::string m_ClosedXmlString = "Closed";
+        
+        const std::map<const std::string, Flagstatemanager> m_FlagXmlStringToStateEnumMap
+        {   {m_OpenXmlString, Flagstatemanager::Open},
+            {m_ClosedXmlString, Flagstatemanager::Closed}
+        };
 
-    enum FLAG_STATE
-(
-    closed
-    open
-);
+        
+		/// @brief  Find or create the state manmanager
+		/// @return IntakeStateMgr* pointer to the state manager
+		static Flagstatemanager* GetInstance();
 
-const std:: m_flagopenstate = ("open")
-const std:: m_flagclosedstate = ("closed")
+        /// @brief Check if driver inputs or sensors trigger a state transition
+        void CheckForStateTransition() override;
+    private:
+
+        Flagstatemanager();
+        ~Flagstatemanager() = default;
+        Flagarm*                               m_Flagarm;
+        std::shared_ptr<nt::NetworkTable>       m_nt;
+
+        static Flagstatemanager* m_instance;
+
+        const double m_CHANGE_STATE_TARGET = 120.0; 
+		static flagstatemanager*	m_instance;
+        const StateStruc m_open = {Flagstatemanager::Open, StateType::Flag_State, true};
+        const StateStruc m_closed = {Flagstatemanager::Closed, StateType::Flag_State, false};
