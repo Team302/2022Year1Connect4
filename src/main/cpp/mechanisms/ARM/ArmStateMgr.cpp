5cbdcc612ff320a67407717364f12d00548495d7
ElliotBayer <72474824+ElliotBayer@users.noreply.github.com>
2022-11-30 20:27:14 -0500

    Changed includes

--- src/main/cpp/mechanisms/ARM/ArmStateMgr.cpp
+++ src/main/cpp/mechanisms/ARM/ArmStateMgr.cpp
@@ -23,19 +23,10 @@
 
 // Team 302 includes
 #include <mechanisms/controllers/MechanismTargetData.h>
-#include <TeleopControl.h>
-#include <hw/DragonLimelight.h>
-#include <hw/factories/LimelightFactory.h>
 #include <mechanisms/MechanismFactory.h>
-#include <mechanisms/MechanismTypes.h>
-#include <mechanisms/base/StateMgr.h>
-#include <mechanisms/StateStruc.h>
-#include <mechanisms/base/IState.h>
-#include <mechanisms\ARM\arm.h>
-//#include <mechanisms/example/ExampleState.h>
-#include <mechanisms\ARM\ArmStateMgr.h>
-#include <utils/Logger.h>
-#include <mechanisms/controllers/StateDataXmlParser.h>
+#include <mechanisms/ARM/arm.h>
+#include <mechanisms/ARM/ArmStateMgr.h>
+#include <TeleopControl.h>
 
 // Third Party Includes
 
